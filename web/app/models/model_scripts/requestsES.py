e719a39b96a7cda3fc09e33b0830505c5466c7e3
root <root@intel-20190621-111949.europe-west1-b.c.smart-rope-243709.internal>
2019-09-18 15:23:53 +0000

    RevMatcher alpha 0.2

--- web/app/models/model_scripts/requestsES.py
+++ web/app/models/model_scripts/requestsES.py
@@ -2,7 +2,7 @@
 
 import pandas as pd
 from elasticsearch import Elasticsearch
-
+import pickle
 
 # CONSTS
 
@@ -22,14 +22,18 @@ DOC_TYPE = 'articles_large'
 
 ## REQUESTS ES
 
-def get_abstracts(es):
+def get_abstracts(es, from_value, size_value):
     res = es.search(index=INDEX_NAME, body={
         "query": {"match_all": {}},
-        "size": 1000,
+        "from": from_value,
+        "size": size_value,
         "_source": ["paperAbstract"]
-    }, request_timeout=50)
+    }, request_timeout=1000)
     res = res['hits']['hits']
 
+    start = from_value + size_value
+    pickle.dump(start, open("app/models/saves/start.p", "wb"))
+    
     df_temp = pd.DataFrame(res)
     return df_temp
 
@@ -37,6 +41,7 @@ def get_abstracts(es):
 ## GET ABSTRACT WITH ID
 
 def get_abstract(es, id):
+    #es = Elasticsearch(hosts=[ES_HOST])
     value = es.search(index=INDEX_NAME, body={
         "query": {"terms": {"_id": [id]}},
         "_source": ["paperAbstract", "authors", "title", "doi"]
@@ -56,3 +61,83 @@ def add_value_es(id, title, keywords, abstract):
         "paperAbstract": abstract
     }
     es.index(index=INDEX_NAME, doc_type=DOC_TYPE, body=line, id=id)
+
+## GET AUTHOR IN ORCID
+
+def get_authors_doi(es, doi):
+    index_name = 'orcid_large'
+
+    res = es.search(index=index_name, body={
+        "query": {
+            "bool": {
+                "should": [
+                    {
+                        "match_phrase": {
+                            "record:record.activities:activities-summary.activities:works.activities:group.common:external-ids.common:external-id.common:external-id-value": str(doi)
+                        }
+                    }
+                ]
+            }
+        },
+        "_source": ["record:record.person:person", "record:record.common:orcid-identifier"]
+    })
+    
+    res = res["hits"]["hits"]
+    
+    return res
+        
+
+def get_authors_name(es, name):
+    index_name = 'orcid_large'
+
+    res = es.search(index=index_name, body={
+        "query": {
+            "bool": {
+                "must": [
+                    {
+                        "bool": {
+                            "should": [
+                                {
+                                    "match_phrase": {
+                                        "record:record.person:person.researcher-url:researcher-urls.researcher-url:researcher-url.common:source.common:source-name": str(name)
+                                    }
+                                },
+                                {
+                                    "match_phrase": {
+                                        "record:record.person:person.person:name.personal-details:credit-name": str(name)
+                                    }
+                                }
+                            ]
+                        }
+                    }
+                ]
+            }
+        },
+        "_source": ["record:record.person:person", "record:record.common:orcid-identifier"]
+    })
+    
+    res = res["hits"]["hits"]
+    
+    return res
+
+def get_authors_orcid(es, orcid):
+    index_name = 'orcid_large'
+    
+    res = es.search(index=index_name, body={
+        "query": {
+            "bool": {
+                "should": [
+                    {
+                        "match_phrase": {
+                           "record:record.common:orcid-identifier.common:path": str(orcid)
+                        }
+                    }
+                ]
+            }
+        },
+        "_source": ["record:record.person:person", "record:record.common:orcid-identifier"]
+    })
+    
+    res = res["hits"]["hits"]
+
+    return res
