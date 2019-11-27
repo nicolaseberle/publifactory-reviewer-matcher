a921c9e556d374cf933af70e38be162e1bd8d64f
Vincent Schuck <vschuck.pro@gmail.com>
2019-11-27 12:21:23 +0100

    ajout test multi similarity

--- web/app/app.py
+++ web/app/app.py
@@ -385,6 +385,7 @@ def request_reviewer_multi():
     abstr = request.args.get('abstract')
     auth = request.args.getlist('authors')
     fields = request.args.getlist('fields')
+    fields = fields.split(",")
     _results = []
     for field in fields:
         dictionary = pickle.load(open("app/models/similarities/"+field+"/dictionary.pkl", "rb"))
