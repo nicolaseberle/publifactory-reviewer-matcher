a9f4262a603e8935bc4838aa2ac933d5a3fedb95
Vincent Schuck <vschuck.pro@gmail.com>
2019-11-27 12:39:30 +0100

    ajout test multi similarity

--- web/app/app.py
+++ web/app/app.py
@@ -399,7 +399,8 @@ def request_reviewer_multi():
 def get_results_multi(job_keys):
     if conn:
         _results = []
-        for index, key in job_keys:
+        job_keys = job_keys.split(",")
+        for key in job_keys:
             job = Job.fetch(key, connection=conn)
             while not job.is_finished:
                 time.sleep(1)
