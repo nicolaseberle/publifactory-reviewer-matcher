d3f761ecac2bcdd4c6c37dc373adf0bf13296461
Vincent Schuck <vschuck.pro@gmail.com>
2019-12-03 15:43:14 +0100

    similarity citation create and delete temp folders

--- web/app/models/model.py
+++ web/app/models/model.py
@@ -168,7 +168,7 @@ def getReviewersCits(es, abstract, authors, sub_cat):
 
     if result != []:
 
-        os.makedirs('/app/models/similarities/' + str(result[0]) + '/indices', 0o753)
+        os.mkdir('/app/models/similarities/' + str(result[0]))
 
         # REQUEST ES
         df_temp = pd.DataFrame(columns=["_id", "_index", "_score", "_source", "_type"])
